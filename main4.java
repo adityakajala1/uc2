4d85d2a89c6a5253ea55ac495175f611e559d11b
adityakajala <ak2285@srmist.edu.in>
2026-03-18 00:21:08 +0530

    UC4: Display Hello with multiple names

---
+++ main4.java
@@ -0,0 +1,12 @@
+public class Main4 {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.print("Hello ");
+            for (int i = 0; i < args.length; i++) {
+                System.out.print(args[i] + " ");
+            }
+        } else {
+            System.out.println("Hello World");
+        }
+    }
+}
